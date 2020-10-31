6f7cdf8fa5b3953e9e048400dda795e56563a5fc
Ira Levkovich <6346390@mail.ru>
2020-11-01 02:31:38 +0300

    Create Italic task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Italic from "./Tasks/Italic";
+import Props from "./Tasks/Props";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Italic />
+      <Props />
     </div>
   );
 }
---
+++ src/Tasks/Props.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Styles.css";
+
+class Props extends React.Component { 
+  render() { 
+    return (
+      <div className="squares">
+        
+      </div>
+    )
+  }
+
+}
+
+export default Props;
