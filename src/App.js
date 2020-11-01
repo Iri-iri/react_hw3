80346cdfc7f07f9f2659d19432a06ee2b67ea952
Ira Levkovich <6346390@mail.ru>
2020-11-01 10:52:20 +0300

    Task Square

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import './App.css';
 import Italic from "./Tasks/Italic";
-import Props from "./Tasks/Props";
+import Square from "./Tasks/Square";
 
 
 function App() {
   return (
     <div className="App">
       <Italic />
-      <Props />
+      
+        <Square label="big" cssClass="big_square"></Square>
+        <Square label="medium" cssClass="medium_square"></Square>
+        <Square label="small" cssClass="small_square"></Square>
     </div>
   );
 }
